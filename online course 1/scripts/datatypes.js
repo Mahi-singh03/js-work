e97cad7e98103dc060beadad66aa05c4cacecbf8
mahi-singh03 <aka.mahisingh@gmail.com>
2024-08-18 20:31:49 +0530

    hi

---
+++ online course 1/scripts/datatypes.js
@@ -0,0 +1,29 @@
+// PREMITIVE DATATYPES ARE OF 7 TYPES :
+
+// Number
+// String
+// Boolean
+// undefined
+// null
+// BigInt
+// Symbol
+
+//NON PREMITIVE DATATYPES ARE :
+
+// 1) OBJECT
+// IS IS COLLECTIONS OF NON PREMITIVE DATATYPES IN FORM OF KEY VALE PAIRS
+
+const my_self = {
+    FullName : "mahi singh",
+    gender : "male",
+    age : 22,
+};
+// KEY VALUE PAIR  OF OBJECT CAN BE UPDATED
+my_self.age = my_self.age+10;
+my_self.FullName = "Manoj";
+
+
+console.log(my_self);
+console.log(typeof(my_self));
+console.log(my_self.age);
+console.log(typeof(my_self.age));
