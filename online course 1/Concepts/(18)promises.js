583f3073b6da4883e42b724296e6ea83e75996e6
raviseehra09 <raviseehra878@gmail.com>
2024-08-30 01:17:23 +0530

    hi

---
+++ online course 1/Concepts/(18)promises.js
@@ -0,0 +1,52 @@
+//                                    Promises
+
+// Promises are objects in Js
+// promises is for "eventual" completion of task
+// A JavaScript Promise object can be:
+// [1] Pending
+// [2] Fulfilled
+// [3] Rejected
+
+// The Promise object supports two properties: state and result.
+
+// While a Promise object is "pending" (working), the result is undefined.
+// When a Promise object is "fulfilled", the result is a value.
+// When a Promise object is "rejected", the result is an error object.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
