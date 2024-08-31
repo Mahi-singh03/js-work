b3431c7606fc2fb147c8eb54b380428575b1306d
raviseehra09 <raviseehra878@gmail.com>
2024-09-01 02:13:42 +0530

    hi

--- online course 1/Concepts/(21)API.js
+++ online course 1/Concepts/(21)API.js
@@ -1,1 +1,6 @@
 //                                     API
+
+// API stands for application program interface.
+// Fatch API provide an interface  for sending and receiving resourses
+// APIs provide the data of the particular subject API related to
+// APIs used Request and Respone objects
