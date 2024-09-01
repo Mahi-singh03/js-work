b64758185235904115f546ea859dae5a4fd38e5d
raviseehra09 <raviseehra878@gmail.com>
2024-09-01 21:45:08 +0530

    hi

--- online course 1/Concepts/(21)API.js
+++ online course 1/Concepts/(21)API.js
@@ -1,6 +1,25 @@
 //                                     API
 
-// API stands for application program interface.
-// Fatch API provide an interface  for sending and receiving resourses
-// APIs provide the data of the particular subject API related to
-// APIs used Request and Respone objects
+// [1] API stands for application program interface.
+// [2] Fatch API provide an interface  for sending and receiving resourses
+// [3] APIs provide the data of the particular subject API related to
+// [4] APIs used Request and Respone objects
+
+
+// [5] Application Programming Interfaces (APIs) are constructs made available in programming
+ //languages to allow developers to create complex functionality more easily. They a
+ //bstract more complex code away from you, providing some easier syntax to use in its place.
+//
+
+
+
+// [6] Browser APIs are built into your web browser and are able to expose data from the browser
+ //and surrounding computer environment and do useful complex things with it. For example, 
+ //the Web Audio API provides JavaScript constructs for manipulating audio in the browser — taking 
+ //an audio track, altering its volume, applying effects to it, etc. In the background, the
+ // browser is actually using some complex lower-level code (e.g. C++ or Rust) to do the 
+ //actual audio processing. But again, this complexity is abstracted away from you by the API.
+// [7] Third-party APIs are not built into the browser by default, and you generally have to retrieve 
+ //their code and information from somewhere on the Web. For example, the Google Maps API allows 
+ //you to do things like display an interactive map to your office on your website. It provides a
+ //special set of constructs you can use to query the Google Maps service and return specific information.
