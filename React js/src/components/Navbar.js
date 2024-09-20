1ad21d0afed55e36c3d041dcd10329ea1a295a9e
raviseehra09 <raviseehra878@gmail.com>
2024-09-20 23:09:11 +0530

    20-9-2024

--- React js/src/components/Navbar.js
+++ React js/src/components/Navbar.js
@@ -1,7 +1,4 @@
-import React, { useState } from 'react';
-
 export default function Navbar(props) {
-
   const mystyle = {
     backgroundColor: "skyBlue",
     height: 30,
@@ -14,11 +11,53 @@ export default function Navbar(props) {
   return (
     <>
       <nav className={`navbar navbar-${props.mode} bg-${props.mode}`}>
-        <div className="container">
-          <a className="navbar-brand" href="#">{props.title}</a>
-          <div className= {`form-check form-switch text-${props.mode==="dark"?"light":"dark"}`}>
-            <input className=" form-check-input"  onClick={props.OC} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-            <label className="form-check-label "  htmlFor="flexSwitchCheckDefault">Dark mode</label>
+        <div className="container d-flex justify-content-between align-items-center">
+          <a className="navbar-brand" href="#">
+            {props.title}
+          </a>
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "dark" ? "light" : "dark"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              onClick={props.OC}
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Dark mode
+            </label>
+          </div>
+          <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+            Thems
+          </button>
+        </div>
+
+        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <div>
+              Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+            </div>
+            <div className="dropdown mt-3">
+              <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                Dropdown button
+              </button>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
