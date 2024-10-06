93dc6dea3ff18de2c33cdc0b1fc6ed764b254f0a
mahi singh <aka.mahisingh@gmail.com>
2024-10-06 15:13:03 +0530

    6-10-24

--- React js/App 1/src/App.js
+++ React js/App 1/src/App.js
@@ -1,9 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Textbox from './components/Textbox';
 
 function App() {
+  
+  
+ const  [ mode , setMode ] = useState('light')
+
+ const OC = () =>{
+    let body = document.querySelector("body");
+    let head = document.querySelector("h1");
+    let head2 = document.querySelectorAll("h5");
+    let lab = document.querySelector(".form-label");
+ 
+  if(mode === 'light'){
+    setMode ('dark')
+      body.style.backgroundColor = "rgb(0, 0, 0)";
+      head.style.color = "white";
+      head2.forEach(h5 => h5.style.color = "white");
+      lab.style.color = "white";
+  }
+  else{
+    setMode ('light')
+    body.style.backgroundColor = "rgb(246, 237, 222)";
+    head.style.color = "black";
+    head2.forEach(h5 => h5.style.color = "black");
+    lab.style.color = "black";
+  }
+ }
+
+  
+
+
   return (
-    <p>hello</p>
+  <>
+  <Navbar title="Text Tools"  mode={mode} OC={OC} />
+  <Textbox headings="Enter the Text hear "  mode={mode} />
+  </>
   );
 }
 
