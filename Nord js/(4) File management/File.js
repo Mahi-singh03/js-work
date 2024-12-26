8051a4fb8105c55927f8ade46a321536356ba02f
mahi singh <aka.mahisingh@gmail.com>
2024-12-26 23:21:43 +0530

    Refactor file management code and update documentation; remove unused glossary file

--- Nord js/(4) File management/File.js
+++ Nord js/(4) File management/File.js
@@ -1,3 +1,110 @@
-const x = require('process')
+// TO CREATE THE FILE USING THE COMMAND LINE ARGUMENTS
 
-console.log(x)
+// const x = process.argv
+// const y = require('fs')
+
+// y.writeFileSync(x[2],x[3]) // fie=rst argument is file name and second argument is content
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TO CREATE AND DELETE THE FILE USING THE COMMAND LINE ARGUMENTS
+
+// const x = process.argv
+// const y = require('fs')
+
+// if(x[2] === 'create'){
+//     y.writeFileSync(x[3],x[4])}
+// else if(x[2] === 'delete'){
+//     y.unlinkSync(x[3])
+// }else{
+//     console.log('Invalid command')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CREATING THE MULTIPLE FILES USING THE COMMAND LINE ARGUMENTS
+
+// const fs = require('fs')
+// const path = require('path')
+// const args = process.argv
+// const dirPath = path.join(__dirname, 'CREATED FILES')
+
+// // Log the arguments to ensure they are being read correctly
+// console.log('Arguments:', args)
+
+
+// for (let i = 0; i < 5; i++) {
+//     const filePath = `${dirPath}/${args[2]}${i}.txt`
+//     fs.writeFileSync(filePath, args[3])
+//     console.log(`File created: ${filePath}`)
+// }
+
+
+// command line COMMAND : node File mahi "this is my sample text 2"
+
+
+
+
+
+
+
+
+
+// CURD OPERATION USING THE COMMAND LINE ARGUMENTS
+
+
+const x = require('fs')
+const y = require('path')
+const z = process.argv
+const dirpath = y.join(__dirname,'CURD')
+
+
+
+// create a file
+
+// x.writeFileSync(`${dirpath}/${z[2]}.txt`, z[3])
+
+// command line COMMAND : node File mahi " this file is creat using creare command"
+
+
+
+// read the file
+
+// const m = x.readFileSync(`${dirpath}/${z[2]}.txt`,'utf-8')
+// console.log(m)
+
+
+
+
+//updeate the file
+
+// x.appendFileSync(`${dirpath}/${z[2]}.txt`, z[3])    
+
+
+
+
+//Delete the file
+
+x.unlinkSync(`${dirpath}/${z[2]}.txt`)   
--- Nord js/(4) File management/NOtes.md
+++ Nord js/(4) File management/NOtes.md
@@ -5,4 +5,35 @@ The process object provides information about, and control over, the current Nod
 
 1) process.argv
 
-this method give standered teo outputs
+* it dont need to be require.
+* it five two standered output : location of the node exe. and location of the file you are in.
+
+
+Fs module
+--
+
+* this is the core module therfore it dont need to install.
+* it needed to be require.
+* different module object are;
+    
+        * fs.WriteFileSync('filenmae.txt','content of the file')
+        * fs.UnlinkSybc('filename.txt')
+        * fs.readFileSync('filename.txt')
+        * fs.appendFileSync('filename')
+    
+        
+        
+        
+Path module
+--
+* this module is needed to be require
+* this gives the path of the directory
+* different object in the path module:
+
+       * path.joim(__dirname, 'name of the directory')
+
+
+CURD   command
+--
+
+* CURD  stand for create, update, read and delete.
